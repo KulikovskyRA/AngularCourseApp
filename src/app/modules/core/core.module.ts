c00ff43905b4b22d31f470ee31fa34d3c864a381
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-10-21 19:28:45 +0300

    footer, bread

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LogoComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    BreadcrumbsComponent,
+    SectionComponent,
+  ],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    SectionComponent,
+  ],
 })
 export class CoreModule {}
