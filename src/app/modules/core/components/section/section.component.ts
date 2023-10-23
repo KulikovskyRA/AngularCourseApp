c46cf7972bf643be001fff97fe819f65ff3fecd2
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-10-23 14:27:52 +0300

    Подраздел 1 - Страница курсов

--- src/app/modules/core/components/section/section.component.ts
+++ src/app/modules/core/components/section/section.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss'],
 })
-export class SectionComponent {}
+export class SectionComponent {
+  value = '';
+  setValue(event: any) {
+    this.value = event.target.value;
+  }
+
+  logValue() {
+    console.log(this.value);
+  }
+}
