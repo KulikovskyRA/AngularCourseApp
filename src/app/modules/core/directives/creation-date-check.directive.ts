4e3061b5fe554e1fbc45e78e5b56ffbefd8466d4
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-10-25 10:45:17 +0300

    dateCardColourFix

--- src/app/modules/core/directives/creation-date-check.directive.ts
+++ src/app/modules/core/directives/creation-date-check.directive.ts
@@ -12,7 +12,8 @@ import * as moment from 'moment';
   selector: '[appCreationDateCheck]',
 })
 export class CreationDateCheckDirective implements AfterViewInit {
-  @Input('appCreationDateCheck') creationDate = '';
+  @Input('appCreationDateCheck')
+  creationDate!: Date;
 
   constructor(
     private readonly element: ElementRef,
@@ -21,8 +22,7 @@ export class CreationDateCheckDirective implements AfterViewInit {
 
   public ngAfterViewInit(): void {
     if (
-      new Date(moment(this.creationDate, 'MM-DD-YYYY', false).format()) <
-        new Date() &&
+      this.creationDate < new Date() &&
       new Date(
         moment(this.creationDate, 'MM-DD-YYYY', false).add(14, 'days').format()
       ) >= new Date()
@@ -31,10 +31,7 @@ export class CreationDateCheckDirective implements AfterViewInit {
       this.renderer.setStyle(child, 'border', '1px solid #22ff00');
     }
 
-    if (
-      new Date(moment(this.creationDate, 'MM-DD-YYYY', false).format()) >
-      new Date()
-    ) {
+    if (this.creationDate > new Date()) {
       const [child] = this.element.nativeElement.children;
       this.renderer.setStyle(child, 'border', '1px solid blue');
     }
